07f4e305770d53e1f4c2e16616652eef086c1dc2
ThiyagarajanR <ThiyagarajanR@users.noreply.github.com>
2025-02-12 22:05:14 +0000

    TaskManager using PriorityQueue

--- Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a62c70a6dbd4dbbf673b80c6693835219326225f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1147a420a7c6c8ca837406a09a9b42a86f485920")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TaskManager/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace TaskManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<Task, int> taskQueue = new PriorityQueue<Task, int>(new PriorityComparer());
+
+            taskQueue.Enqueue(new Task("Task 1", Priority.Low), (int)Priority.Low);
+            taskQueue.Enqueue(new Task("Task 2", Priority.Medium), (int)Priority.Medium);
+            taskQueue.Enqueue(new Task("Task 3", Priority.High), (int)Priority.High);
+            taskQueue.Enqueue(new Task("Task 4", Priority.Low), (int)Priority.Low);
+            taskQueue.Enqueue(new Task("Task 5", Priority.Medium), (int)Priority.Medium);
+            taskQueue.Enqueue(new Task("Task 6", Priority.High), (int)Priority.High);
+            taskQueue.Enqueue(new Task("Task 7", Priority.Low), (int)Priority.Low);
+            taskQueue.Enqueue(new Task("Task 8", Priority.Medium), (int)Priority.Medium);
+            taskQueue.Enqueue(new Task("Task 9", Priority.High), (int)Priority.High);
+            taskQueue.Enqueue(new Task("Task 10", Priority.Low), (int)Priority.Low);
+            taskQueue.Enqueue(new Task("Task 11", Priority.Medium), (int)Priority.Medium);
+
+            while (taskQueue.Count > 0)
+            {
+                Task task = taskQueue.Dequeue();
+                Console.WriteLine($"{task.Description} {task.Priority}");
+            }
+        }
+    }
+
+    class PriorityComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            return y.CompareTo(x);
+        }
+    }
+
+    enum Priority
+    {
+        Low = 1,
+        Medium,
+        High
+    }
+
+    class Task
+    {
+        /// <summary>
+        /// Gets the description of the task.
+        /// </summary>
+        public string Description { get; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Task"/> class.
+        /// </summary>
+        /// <param name="description">The description of the task.</param>
+        /// <param name="priority">The priority of the task.</param>
+        
+         /// <summary>
+        /// Gets the priority of the task.
+        /// </summary>
+        public Priority Priority { get; }
+        public Task(string description, Priority priority)
+        {
+            Description = description;
+            Priority = priority;
+        }
+       
+
+        
+    }
+}
